373dfc1143f8d6a8f850ef7adab6b42865a826db
Casey Leonard <casey@amphibian.com>
2012-11-18 14:36:25 -0500

    code cleanup

--- src/main/java/edu/psu/sweng/ff/client/Members.java
+++ src/main/java/edu/psu/sweng/ff/client/Members.java
@@ -93,27 +93,6 @@ public class Members {
 	
 	}
 
-//	public static Member getByUserId(String id) {
-//		
-//		String url = BASE_URL + "id/" + id;
-//	
-//		WebResource r = c.resource(url);
-//		ClientResponse response = r.header(TOKEN_HEADER, userToken)
-//				.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-//		
-//		Member m = null;
-//		if (response.getStatus() == Status.OK.getStatusCode()) {
-//			String json = response.getEntity(String.class);
-//			Gson gson = new Gson();
-//			m = gson.fromJson(json, Member.class);
-//		} else {
-//			System.err.println(response);
-//		}
-//		
-//		return m;
-//	
-//	}
-
 	public static Member update(Member m) {
 		
 		String url = BASE_URL + m.getUserName();
